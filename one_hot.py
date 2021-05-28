3161808403cef8277e4664629988491d05f8c202
EthanLifeGreat <45615462+EthanLifeGreat@users.noreply.github.com>
2021-05-28 21:28:06 +0800

    Delete one_hot.py

--- one_hot.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-
-
-def encode_one_hot(y):
-    _num_samples = len(y)
-    _num_classes = np.max(y) + 1
-    _y_01 = np.zeros([_num_samples, _num_classes])
-    _y_01[np.arange(0, y.shape[0]), y.ravel()] = 1
-    return _y_01
